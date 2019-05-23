4ab211e81dec858d290499c25b818c32183fab96
wijdan1995 <wijdan.w.k@outlook.sa>
2019-05-23 15:07:16 +0300

    Setup devise to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/home", to: "home#index"
+
+  root "home#index"
+
+
 end
