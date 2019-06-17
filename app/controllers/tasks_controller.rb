f5ad5a1e1a0caf64861e8bf8b8f62d9363d1d541
wijdan1995 <wijdan.w.k@outlook.sa>
2019-06-17 14:09:17 +0300

    tasks done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+    def create
+        @project = Project.find(params[:project_id])
+        @task = @project.tasks.create(task_params)
+        redirect_to project_path(@project)
+    end
+
+    def destroy
+        @project = Project.find(params[:project_id])
+        @task = @project.tasks.find(params[:id])
+        @task.destroy
+        redirect_to project_path(@project)
+    end
+     
+    private
+        def task_params
+            params.require(:task).permit(:content)
+        end
+
+
 end
