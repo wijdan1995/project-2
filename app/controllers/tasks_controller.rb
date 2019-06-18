c5cef00124d249a978979fa135fd33869b2356ab
wijdan1995 <wijdan.w.k@outlook.sa>
2019-06-18 16:02:20 +0300

    added Validation and background

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
+    before_action :set_project
+
     def create
-        @project = Project.find(params[:project_id])
         @task = @project.tasks.create(task_params)
         redirect_to project_path(@project)
     end
-
+    
     def destroy
-        @project = Project.find(params[:project_id])
         @task = @project.tasks.find(params[:id])
         @task.destroy
         redirect_to project_path(@project)
     end
      
     private
+
+        def set_project
+            @project = Project.find(params[:project_id])
+        end
         def task_params
             params.require(:task).permit(:content)
         end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks , dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
